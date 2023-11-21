553743241e41817797cbe125f9293d503819d2fe
rota.20196 <rota.20196@E2-9-PC11>
2023-11-21 12:25:28 +0100

    <blocco dimensione minima e massima form>

--- Classe articoli/Form1.Designer.cs
+++ Classe articoli/Form1.Designer.cs
@@ -28,79 +28,87 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panelSelection = new System.Windows.Forms.Panel();
-			this.buttonAdd = new System.Windows.Forms.Button();
-			this.panelTitle = new System.Windows.Forms.Panel();
-			this.labelTitle = new System.Windows.Forms.Label();
-			this.panelContent = new System.Windows.Forms.Panel();
-			this.panelSelection.SuspendLayout();
-			this.panelTitle.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// panelSelection
-			// 
-			this.panelSelection.BackColor = System.Drawing.SystemColors.AppWorkspace;
-			this.panelSelection.Controls.Add(this.buttonAdd);
-			this.panelSelection.Controls.Add(this.panelTitle);
-			this.panelSelection.Dock = System.Windows.Forms.DockStyle.Left;
-			this.panelSelection.Location = new System.Drawing.Point(0, 0);
-			this.panelSelection.Name = "panelSelection";
-			this.panelSelection.Size = new System.Drawing.Size(195, 582);
-			this.panelSelection.TabIndex = 0;
-			// 
-			// buttonAdd
-			// 
-			this.buttonAdd.Dock = System.Windows.Forms.DockStyle.Top;
-			this.buttonAdd.FlatAppearance.BorderSize = 0;
-			this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonAdd.Location = new System.Drawing.Point(0, 88);
-			this.buttonAdd.Name = "buttonAdd";
-			this.buttonAdd.Size = new System.Drawing.Size(195, 53);
-			this.buttonAdd.TabIndex = 1;
-			this.buttonAdd.Text = "Aggiungi";
-			this.buttonAdd.UseVisualStyleBackColor = true;
-			this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
-			// 
-			// panelTitle
-			// 
-			this.panelTitle.Controls.Add(this.labelTitle);
-			this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
-			this.panelTitle.Location = new System.Drawing.Point(0, 0);
-			this.panelTitle.Name = "panelTitle";
-			this.panelTitle.Size = new System.Drawing.Size(195, 88);
-			this.panelTitle.TabIndex = 0;
-			// 
-			// labelTitle
-			// 
-			this.labelTitle.AutoSize = true;
-			this.labelTitle.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelTitle.Location = new System.Drawing.Point(12, 33);
-			this.labelTitle.Name = "labelTitle";
-			this.labelTitle.Size = new System.Drawing.Size(161, 21);
-			this.labelTitle.TabIndex = 0;
-			this.labelTitle.Text = "Gestione Articoli";
-			// 
-			// panelContent
-			// 
-			this.panelContent.Dock = System.Windows.Forms.DockStyle.Right;
-			this.panelContent.Location = new System.Drawing.Point(192, 0);
-			this.panelContent.Name = "panelContent";
-			this.panelContent.Size = new System.Drawing.Size(964, 582);
-			this.panelContent.TabIndex = 1;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1156, 582);
-			this.Controls.Add(this.panelContent);
-			this.Controls.Add(this.panelSelection);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.panelSelection.ResumeLayout(false);
-			this.panelTitle.ResumeLayout(false);
-			this.panelTitle.PerformLayout();
-			this.ResumeLayout(false);
+            this.panelSelection = new System.Windows.Forms.Panel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelSelection.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSelection
+            // 
+            this.panelSelection.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panelSelection.Controls.Add(this.buttonAdd);
+            this.panelSelection.Controls.Add(this.panelTitle);
+            this.panelSelection.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSelection.Location = new System.Drawing.Point(0, 0);
+            this.panelSelection.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelSelection.Name = "panelSelection";
+            this.panelSelection.Size = new System.Drawing.Size(146, 473);
+            this.panelSelection.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonAdd.FlatAppearance.BorderSize = 0;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdd.Location = new System.Drawing.Point(0, 72);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(146, 43);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Aggiungi";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.Controls.Add(this.labelTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(146, 72);
+            this.panelTitle.TabIndex = 0;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(9, 27);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(131, 17);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Gestione Articoli";
+            // 
+            // panelContent
+            // 
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelContent.Location = new System.Drawing.Point(144, 0);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(723, 473);
+            this.panelContent.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 473);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelSelection);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(883, 512);
+            this.MinimumSize = new System.Drawing.Size(883, 512);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelSelection.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.ResumeLayout(false);
 
 		}
 
